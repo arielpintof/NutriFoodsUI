f8b2752b136e6309e00fb51ba7187fc0885117eb
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-09-27 14:47:24 -0300

    Configuraciones generales

--- NutriFoods-UI/Program.cs
+++ NutriFoods-UI/Program.cs
@@ -1,12 +1,19 @@
+using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NutriFoods_UI;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
+var currentAssembly = typeof(Program).Assembly;
+builder.Services.AddFluxor(options => options.ScanAssemblies(currentAssembly));
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+
+
