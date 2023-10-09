231c225f0edda6251b21523fb93e1c2a3bff2f14
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-10-09 18:35:39 -0300

    Create MicroNutrientDto.cs

---
+++ NutriFoods-UI/Data/Dto/MicroNutrientDto.cs
@@ -0,0 +1,10 @@
+﻿using NutriFoods_UI.Utils.Enums;
+
+namespace NutriFoods_UI.Data.Dto;
+
+public class MicroNutrientDto
+{
+    public string? Name { get; set; }
+    public double? Quantity { get; set; } 
+    public UnitEnum Measure { get; set; } = null!;
+}
