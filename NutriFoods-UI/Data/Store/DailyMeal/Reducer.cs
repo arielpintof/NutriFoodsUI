fe1885acb3a8918b1f3c91580c09babc9e9b74e9
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-10-16 00:19:23 -0300

    Minor update daily meal reducer

--- NutriFoods-UI/Data/Store/DailyMeal/Reducer.cs
+++ NutriFoods-UI/Data/Store/DailyMeal/Reducer.cs
@@ -6,7 +6,7 @@ namespace NutriFoods_UI.Data.Store.DailyMeal;
 public class Reducer
 {
     [ReducerMethod]
-    public static DailyMealState ReduceChangeMoleculaAction(DailyMealState state, ChangeDailyMealAction action)
+    public static DailyMealState ChangeDailyMenu(DailyMealState state, ChangeDailyMealAction action)
     {
         var updatedDailyMenu = state.DailyMenu.ToList();
         updatedDailyMenu[action.Position] = action.DailyMenu;
