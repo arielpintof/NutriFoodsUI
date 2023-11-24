85cd5e885211f62c37a36d7fa0361a46040d611f
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-11-24 13:15:42 -0300

    update enums

---
+++ NutriFoods-UI/Utils/Enums/ConsultationTypes.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.SmartEnum;
+
+namespace NutriFoods_UI.Utils.Enums;
+
+public class ConsultationTypes : SmartEnum<ConsultationTypes>, IEnum<ConsultationTypes, ConsultationTypeToken>
+{
+    public static readonly ConsultationTypes None =
+        new(nameof(None), (int)ConsultationTypeToken.None, string.Empty);
+
+    public static readonly ConsultationTypes Evaluation =
+        new(nameof(Evaluation), (int)ConsultationTypeToken.Evaluation, "Evaluación nutricional");
+
+    public static readonly ConsultationTypes Control =
+        new(nameof(Control), (int)ConsultationTypeToken.Control, "Seguimiento nutricional");
+
+    private ConsultationTypes(string name, int value, string readableName) : base(name, value) => ReadableName = readableName;
+
+    public string ReadableName { get; }
+}
+
+public enum ConsultationTypeToken
+{
+    None,
+    Evaluation,
+    Control
+}
--- NutriFoods-UI/Utils/Enums/InheritanceTypes.cs
+++ NutriFoods-UI/Utils/Enums/InheritanceTypes.cs
@@ -2,24 +2,24 @@
 
 namespace NutriFoods_UI.Utils.Enums;
 
-public class Inheritance : SmartEnum<Inheritance>, IEnum<Inheritance, InheritanceToken>
+public class InheritanceTypes : SmartEnum<InheritanceTypes>, IEnum<InheritanceTypes, InheritanceToken>
 {
-    public static readonly Inheritance None =
+    public static readonly InheritanceTypes None =
         new(nameof(None), (int)InheritanceToken.None, string.Empty);
 
-    public static readonly Inheritance Direct =
+    public static readonly InheritanceTypes Direct =
         new(nameof(Direct), (int)InheritanceToken.Direct, "Afección directa");
 
-    public static readonly Inheritance Paternal =
+    public static readonly InheritanceTypes Paternal =
         new(nameof(Paternal), (int)InheritanceToken.Paternal, "Del lado paterno");
 
-    public static readonly Inheritance Maternal =
+    public static readonly InheritanceTypes Maternal =
         new(nameof(Maternal), (int)InheritanceToken.Maternal, "Del lado materno");
 
-    public static readonly Inheritance Siblings =
+    public static readonly InheritanceTypes Siblings =
         new(nameof(Siblings), (int)InheritanceToken.Siblings, "Hermanos");
 
-    private Inheritance(string name, int value, string readableName) : base(name, value) => ReadableName = readableName;
+    private InheritanceTypes(string name, int value, string readableName) : base(name, value) => ReadableName = readableName;
 
     public string ReadableName { get; }
 }
