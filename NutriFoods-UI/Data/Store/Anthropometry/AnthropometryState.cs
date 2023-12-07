68869ea4809305be26e00a0a8f8a6965cd90aa1a
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-07 00:44:00 -0300

    wrapped all anthr inputs in one component

---
+++ NutriFoods-UI/Data/Store/Anthropometry/AnthropometryState.cs
@@ -0,0 +1,17 @@
+﻿using Fluxor;
+using NutriFoods_UI.Data.Dto;
+
+namespace NutriFoods_UI.Data.Store.Anthropometry;
+
+[FeatureState]
+public class AnthropometryState
+{
+    public AnthropometryDto Anthropometry { get; set; } = new();
+    
+    public AnthropometryState(){}
+
+    public AnthropometryState(AnthropometryDto anthropometry)
+    {
+        Anthropometry = anthropometry;
+    }
+}
