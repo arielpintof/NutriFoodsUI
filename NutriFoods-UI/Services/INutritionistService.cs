b58aa3dd00deaedff938c2d82bc3f3e3fd680c11
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-11 13:57:33 -0300

    add nutritionist and patients endpoints

---
+++ NutriFoods-UI/Services/INutritionistService.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NutriFoods_UI.Data;
+using NutriFoods_UI.Data.Dto;
+
+namespace NutriFoods_UI.Services;
+
+public interface INutritionistService
+{
+
+    Task<HttpResponseMessage?> SignUp(NutritionistDto dto);
+
+    Task<HttpResponseMessage?> Login([Required] string email, [Required] string password);
+
+    Task<HttpResponseMessage?> AddPatient(Guid nutritionistId, PatientDto patientDto);
+
+}
