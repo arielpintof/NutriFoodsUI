b47162483e7781b22d607d446f694d95660906b3
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-12 13:16:36 -0300

    remove empty space

--- NutriFoods-UI/Data/Dto/PatientDto.cs
+++ NutriFoods-UI/Data/Dto/PatientDto.cs
@@ -1,12 +1,14 @@
+using System.Text.Json.Serialization;
 using NutriFoods_UI.Data.Dto;
 using NutriFoods_UI.Data.Dto.Abridged;
 using NutriFoods_UI.Data.Model;
 
 namespace NutriFoods_UI.Data;
 
-public sealed class  PatientDto
+public sealed class PatientDto
 {
     public Guid Id { get; set; }
+    
     public DateTime JoinedOn { get; set; }
     public PersonalInfoDto? PersonalInfo { get; set; }
     public ContactInfoDto? ContactInfo { get; set; }
