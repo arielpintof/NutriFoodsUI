044acaacca77d1f53740e270976832ff4e743333
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-12 19:27:34 -0300

    fix object type collection from consultations in patientdto

--- NutriFoods-UI/Data/Dto/PatientDto.cs
+++ NutriFoods-UI/Data/Dto/PatientDto.cs
@@ -5,7 +5,7 @@ using NutriFoods_UI.Data.Model;
 
 namespace NutriFoods_UI.Data;
 
-public sealed class PatientDto
+public class PatientDto
 {
     public Guid Id { get; set; }
     
@@ -13,5 +13,5 @@ public sealed class PatientDto
     public PersonalInfoDto? PersonalInfo { get; set; }
     public ContactInfoDto? ContactInfo { get; set; }
     public AddressDto? Address { get; set; }
-    public ICollection<Consultation> Consultations { get; set; } = null!;
+    public List<ConsultationDto> Consultations { get; set; } = null!;
 }
