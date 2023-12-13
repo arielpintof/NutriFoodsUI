e06060ead14ca60df77cd4e214f5fed2395398e2
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-13 00:25:29 -0300

    add post anthropometry effect

---
+++ NutriFoods-UI/Data/Store/Anthropometry/Effects.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using Fluxor;
+using NutriFoods_UI.Data.Dto;
+using NutriFoods_UI.Services;
+
+namespace NutriFoods_UI.Data.Store.Anthropometry;
+
+public class Effects(
+    IState<AnthropometryState> anthropometryState,
+    IPatientService patientService)
+{
+    [EffectMethod]
+    public async Task PostAnthropometry(PostAnthropometryAction action, IDispatcher dispatcher)
+    {
+        var response = await patientService.AddAnthropometry(
+            action.PatientId, action.ConsultationId, anthropometryState.Value.Anthropometry);
+
+        var content = response.Content.ReadFromJsonAsync<ConsultationDto>();
+        
+        
+    }
+}
