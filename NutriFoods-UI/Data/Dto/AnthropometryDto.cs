f0b7cbaf09631463234b6f5ffab256c4f7763bdf
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-13 01:18:34 -0300

    xd

--- NutriFoods-UI/Data/Dto/AnthropometryDto.cs
+++ NutriFoods-UI/Data/Dto/AnthropometryDto.cs
@@ -4,9 +4,11 @@ public class AnthropometryDto
 {
     public int Height { get; set; }
     public double Weight { get; set; }
+    
     public double Bmi { get; set; }
     public double MuscleMassPercentage { get; set; }
     public double WaistCircumference { get; set; }
+    
     public DateTime? CreatedOn { get; set; }
     public DateTime? LastUpdated { get; set; }
 }
