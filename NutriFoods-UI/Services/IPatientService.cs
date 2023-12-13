7ef4ae91d69a1ebed4bf71a6ae11935716b00eab
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-13 01:18:22 -0300

    Update IPatientService.cs

--- NutriFoods-UI/Services/IPatientService.cs
+++ NutriFoods-UI/Services/IPatientService.cs
@@ -6,21 +6,21 @@ namespace NutriFoods_UI.Services
 {
     public interface IPatientService
     {
-        Task<HttpResponseMessage?> FindPatient(Guid patientId);
+        Task<HttpResponseMessage> FindPatient(Guid patientId);
 
-        Task<HttpResponseMessage?> FindConsultations(Guid patientId);
+        Task<HttpResponseMessage> FindConsultations(Guid patientId);
 
-        Task<HttpResponseMessage?> FindLatestConsultation(Guid patientId);
+        Task<HttpResponseMessage> FindLatestConsultation(Guid patientId);
 
-        Task<HttpResponseMessage?> CreateConsultation(Guid patientId, ConsultationDto consultationDto);
+        Task<HttpResponseMessage> CreateConsultation(Guid patientId, ConsultationDto consultationDto);
 
-        Task<HttpResponseMessage?> AddClinicalAnamnesis(Guid patientId, Guid consultationId, 
+        Task<HttpResponseMessage> AddClinicalAnamnesis(Guid patientId, Guid consultationId, 
             ClinicalAnamnesisDto clinicalAnamnesisDto);
 
-        Task<HttpResponseMessage?> AddNutritionalAnamnesis(Guid patientId, Guid consultationId,
+        Task<HttpResponseMessage> AddNutritionalAnamnesis(Guid patientId, Guid consultationId,
             NutritionalAnamnesisDto nutritionalAnamnesisDto);
 
-        Task<HttpResponseMessage?> AddAnthropometry(Guid patientId, Guid consultationId,
+        Task<HttpResponseMessage> AddAnthropometry(Guid patientId, Guid consultationId,
             AnthropometryDto anthropometryDto);
     }
 }
