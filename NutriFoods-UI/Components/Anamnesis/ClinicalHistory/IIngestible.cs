cef9c9ab6179cfe047b505146dc94f2dfe2e7211
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-22 02:27:56 -0300

    ingestibles now inherits ingestible abstract class

---
+++ NutriFoods-UI/Components/Anamnesis/ClinicalHistory/IIngestible.cs
@@ -0,0 +1,17 @@
+﻿
+using MudBlazor;
+
+namespace NutriFoods_UI.Components;
+
+public interface IIngestible
+{
+    public DialogOptions Options { get; }
+    public void AddIngestible();
+
+    public void EditIngestible(int index);
+    
+    public void DeleteIngestible(int index);
+
+    void LoadIngestible();
+}
+
---
+++ NutriFoods-UI/Components/Anamnesis/ClinicalHistory/IngestibleDialog.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using NutriFoods_UI.Components.Anamnesis.ClinicalHistory;
+using NutriFoods_UI.Data.Dto;
+using NutriFoods_UI.Utils.Enums;
+using NutriFoods_UI.Utils.Enums.Validators.Anamnesis;
+
+namespace NutriFoods_UI.Components;
+
+public abstract class IngestibleDialog : ComponentBase
+{
+    [CascadingParameter]
+    public MudDialogInstance MudDialogInstance { get; set; } = null!;
+
+    public IngestibleDto IngestibleModel { get; set; } = new();
+
+    protected MudForm IngestibleForm { get; set; } = new();
+
+    public IngestibleValidator IngestibleValidator { get; set; } = new();
+
+    [Parameter]
+    public int Index { get; set; }
+
+    [Parameter]
+    public ModeTypeEnum ModeType { get; set; } = null!;
+    
+    public void AddAdministrationTime()
+    {
+        IngestibleModel.AdministrationTimes.Add(new TimeSpan());
+        StateHasChanged();
+    }
+
+    protected static IEnumerable<Frequencies> FrequenciesCollection =>
+        IEnum<Frequencies, FrequencyToken>.TokenDictionary.Select(e => e.Value).Skip(1).ToList();
+
+    protected static IEnumerable<Units> UnitsCollection =>
+        IEnum<Units, UnitToken>.TokenDictionary.Select(e => e.Value).Skip(1).ToList();
+}
