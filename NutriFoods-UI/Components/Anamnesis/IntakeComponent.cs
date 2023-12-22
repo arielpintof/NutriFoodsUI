e2b2a4c3feacec0167474e9719a7829c8ffa5257
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-12-22 02:56:34 -0300

    add IntakeComponent abstract class

---
+++ NutriFoods-UI/Components/Anamnesis/IntakeComponent.cs
@@ -0,0 +1,27 @@
+﻿using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.VisualBasic.FileIO;
+
+namespace NutriFoods_UI.Components.Anamnesis;
+
+public abstract class IntakeComponent : FluxorComponent
+{
+    [Parameter]
+    public int Index { get; set; }
+    
+    protected abstract void Delete();
+
+    protected abstract void Update();
+
+    private bool _isValid;
+    protected bool IsValidHandler
+    {
+        get => _isValid;
+        set
+        {
+            if (_isValid == value) return;
+            _isValid = value;
+            Update();
+        }
+    }
+}
