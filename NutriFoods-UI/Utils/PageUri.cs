e8c99a60a079732307553d15352ac05b308955e8
Ariel Pinto <ariel.pintof@userena.cl>
2024-02-17 01:55:26 -0300

    Create PageUri.cs

---
+++ NutriFoods-UI/Utils/PageUri.cs
@@ -0,0 +1,12 @@
+﻿namespace NutriFoods_UI.Utils.Enums;
+
+public static class PageUri
+{
+    public const string SignUp = "/signup";
+    public const string LogIn = "/login";
+    public const string Home = "/";
+    public const string Dashboard = "/dashboard";
+}
+
+
+
